5380d345012a9769109d0d84930d0d6c1b27b022
r.tefy <r.tefy_24@hotmail.com>
2021-09-02 22:42:43 -0500

    first commtit

---
+++ js/index.js
@@ -0,0 +1,12 @@
+
+
+let typed= new Typed('.typed', {
+    strings: [
+            'Diseño Web Interactivo', 
+            'Produccion Multimedia',
+            'Desarrollo de Software'
+            ],
+    typeSpeed:75,
+    startDelay:500,
+    loop:true
+});
